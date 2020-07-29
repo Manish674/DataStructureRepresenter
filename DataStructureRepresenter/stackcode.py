6015b01daabdd9bf1a5e6089210acc24bf880bbb
Manish674 <63183756+Manish674@users.noreply.github.com>
2020-07-29 09:05:47 +0530

    Add files via upload
    New file!

--- DataStructureRepresenter/stackcode.py
+++ DataStructureRepresenter/stackcode.py
@@ -1,14 +1,2 @@
-content = '''
 
-            val_holder = [ ]
-            no_of_vals = input("How many vlaues you want to enter\n")
-            
-            for i in range(int(no_of_vals)):
-                values = float(input('Enter your value'))
-                val_holder.append(values)
-            
-            
-            print(f"Values before pop {val_holder}")
-            print(f'Last value you inserted {val_holder.pop()}')
-            print(f"Values after pop {val_holder}")
-'''
+
